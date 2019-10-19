6fb2308b736887a77c539ad88114e67c766ebc1c
plaonet <poloteiva@gmail.com>
2019-10-19 17:53:33 +0200

    Added files to the asset + rocket stuff

--- Rocket.js
+++ Rocket.js
@@ -1,18 +1,20 @@
 
 /*Function to update the orbit radius of the rocket*/
-function updateRocketOrbit(){
+function updateRocketOrbit(rocketPath,radius){
     //if left key, decrease radius
     if (cursors.left.isDown)
     {
-        radius-=1;
-        path.destroy();
-        path.add(new Phaser.Curves.Ellipse(400, 300, radius));
+        radius+=1;
+        rocketPath.destroy();
+        rocketPath.add(new Phaser.Curves.Ellipse(650, 300, radius));
     }
     //if right key, increase radius
     if (cursors.right.isDown)
     {
         radius-=1;
-        path.destroy();
-        path.add(new Phaser.Curves.Ellipse(400, 300, radius));
+        rocketPath.destroy();
+        rocketPath.add(new Phaser.Curves.Ellipse(650, 300, radius));
     }
+
+    return radius
 }
